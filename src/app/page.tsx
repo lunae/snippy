20fac44e04def7d5d32d53ed55fb0f9c8d36f539
Manny Luna <emmanuel.luna421@gmail.com>
2024-07-05 16:21:36 -0500

    feat: style home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { db } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex justify-between m-2 items-center">
+        <h1 className="text-xl text-bold">Snippets</h1>
+        <Link href={'/snippets/new'} className="p-2 border rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
